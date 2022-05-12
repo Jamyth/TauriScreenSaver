89c21d78955abdea758302538c6f8c7e7d901dd0
Jamyth <jamyth0712@gmail.com>
2022-05-12 18:10:52 +0800

    [REFACTOR]: added quit menu item

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,13 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{Submenu, Menu, MenuItem};
+
 fn main() {
+  let menu = Menu::new().add_submenu(Submenu::new("appname", Menu::new().add_native_item(MenuItem::Quit)));
   tauri::Builder::default()
     // .invoke_handler(tauri::generate_handler!())
+    .menu(menu)
     .plugin(tauri_plugin_nosleep::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
